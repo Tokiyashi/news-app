0414d32cfd2a862cd2dfbfb4c0875b6a5049026b
Tokiyashi <rogich6319@gmail.com>
2022-06-24 20:30:07 +0300

    Left Sidebar, navigation menu, PostForm

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
 import './index.css';
-import {useState} from "react";
-import Post from "./Components/Post/Post";
 import Header from "./Components/UI/Header/Header";
 import UserProfile from "./Components/UI/UserProfile/UserProfile";
+import PostList from "./Components/PostList/PostList";
 
 function App() {
-    const [posts, setPost] = useState([
-        {name: "Мазила", header:'Прикол', content: 'РЕБЯТА НОЖ ВЫПАЛ! ЧЕСТНО Я НЕ ШУЧУ!'},
-        {name: "Вася", header:'какой-то заголовок', content: 'РЕБЯТА НОЖ ВЫПАЛ! ЧЕСТНО Я НЕ ШУЧУ!'},
-    ]);
-
-
-
-    const postList = posts.map((item, index) =>
-    <Post key={index} header={item.header} author={item.name} content={item.content} > </Post>);
-
   return (
-      <main>
-          <Header/>
-          <UserProfile/>
-          {postList}
-      </main>
+    <>
+        <Header/>
+        <main>
+            <UserProfile/>
+            <PostList/>
+        </main>
+    </>
   );
 }
 
--- src/Components/UI/UserProfile/UserProfile.jsx
+++ src/Components/UI/UserProfile/UserProfile.jsx
@@ -1,43 +1,61 @@
-import React from 'react';
+import React, {useState} from 'react';
 import test from "../../../img/test avatar.jpg";
 import cl from './UserProfile.module.css'
 import {useDispatch, useSelector} from "react-redux";
+import Modal from "../Modal/Modal";
+import SignForm from "../SignForm/SignForm";
+import PostForm from "../PostForm/PostForm";
 
 const UserProfile = () => {
+
+    const [createPostModal, setCreatePostModal] = useState(false);
 
     const dispatch = useDispatch()
     const follows = useSelector(state => state.follows)
 
 
     return (
-        <>
-            <div className={cl.userProfile}>
-                <div className={cl.userProfile__info}>
-                    <div>
-                        <h3 className={cl.userProfile__info__username}> UserName </h3>
-                        <p className={cl.userProfile__info__id}>id:12312415</p>
-                        <p className={cl.userProfile__info__status}>"Волк не тот кто волк, а тот кто не выступает в цирке"</p>
+
+        <div className={cl.wrapper}>
+            <div className={cl.leftBlock} >
+                <button className={cl.leftBlock__button} > Me </button>
+                <button className={cl.leftBlock__button} > News </button>
+                <button className={cl.leftBlock__button} > Followers </button>
+                <button className={cl.leftBlock__button} > Liked </button>
+            </div>
+            <div className={cl.mainBlock}>
+                <div className={cl.userProfile}>
+                    <div className={cl.userProfile__info}>
+                        <div>
+                            <h3 className={cl.userProfile__info__username}> UserName </h3>
+                            <p className={cl.userProfile__info__id}>id:12312415</p>
+                            <p className={cl.userProfile__info__status}>"Волк не тот кто волк, а тот кто не выступает в цирке"</p>
+                        </div>
+                        <button onClick={() => {setCreatePostModal(true)}}> What's New today? </button>
                     </div>
-                    <button> What's New today? </button>
+                    <img className={cl.userProfile__avatar}
+                         alt="avatar"
+                         src={test}
+                         draggable="false"
+                    />
+                    <Modal active={createPostModal} setActive={setCreatePostModal}>
+                        <PostForm/>
+                    </Modal>
+
                 </div>
-                <img className={cl.userProfile__avatar}
-                     alt="avatar"
-                     src={test}
-                     draggable="false"
-                />
+                <div className={cl.followInfo}>
+                    <div className={[cl.followInfo__follows, cl.redBox].join(' ')}>
+                        <h3>Follows: {follows} </h3>
+                    </div>
+                    <div className={[cl.followInfo__followers, cl.redBox].join(' ')}>
+                        <h3 >Followers: 13</h3>
+                    </div>
+                    <div className={[cl.followInfo__likes, cl.redBox].join(' ')}>
+                        <h3> Likes: 342 </h3>
+                    </div>
+                </div>
             </div>
-            <div className={cl.followInfo}>
-                <div className={[cl.followInfo__follows, cl.redBox].join(' ')}>
-                    <h3>Follows: {follows} </h3>
-                </div>
-                <div className={[cl.followInfo__followers, cl.redBox].join(' ')}>
-                    <h3 >Followers: 13</h3>
-                </div>
-                <div className={[cl.followInfo__likes, cl.redBox].join(' ')}>
-                    <h3> Likes: 342 </h3>
-                </div>
-            </div>
-        </>
+        </div>
     );
 };
 
