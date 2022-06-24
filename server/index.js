16f4d78ab24a49743220775d661d3d52ae5864cb
devlsl <vslozhen@gmail.com>
2022-06-24 21:44:59 +0300

    testing server: added test queries to db, added controllers, added routes

--- server/index.js
+++ server/index.js
@@ -1,14 +1,11 @@
-import express from 'express'
-
+const express = require('express')
+const userRouter = require('../routes/user.routes')
 const PORT = 4000;
 
 const app = express()
 
 app.use(express.json())
 
-app.post('/', (req, res) => {
-    console.log(req.body);
-    res.status(200).json('Сервер работает')
-})
+app.use('/api', userRouter)
 
 app.listen(PORT, () => console.log('SERVER STARTED ON PORT ' + PORT))
