2503f27ca65936e0213c1c7820023f0a25c8e93f
devlsl <vslozhen@gmail.com>
2022-06-25 12:55:26 +0300

    added rotes

--- server/index.js
+++ server/index.js
@@ -2,12 +2,19 @@ require('dotenv').config()
 const express = require('express')
 const sequelize = require('./db')
 const models = require('./models/models')
-
+const cors = require('cors')
+const router = require('./routes/index')
 
 const PORT = process.env.PORT || 4000
 
 const app = express()
-app
+app.use(cors())
+app.use(express.json())
+app.use('/api', router)
+
+app.get('/', (req, res) => {
+    res.status(200).json({message: 'Working!!!'})
+})
 
 const start = async () => {
     try {
