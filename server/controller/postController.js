0d0ac2525cfc4526def807f942a4637dc30903d6
devlsl <vslozhen@gmail.com>
2022-06-28 14:21:39 +0300

    done controller and router for post

--- server/controller/postController.js
+++ server/controller/postController.js
@@ -24,11 +24,70 @@ class PostController{
         const id = req.body.id
         const isTherePost = await db.query('select user_id from "post" where id = $1', [id])
         if (isTherePost.rowCount == 1) {
+            // добавить удаление коментов и лайков
 
+            await db.query('delete from "post" where id = $1', [id])
+
+            const postCount = await db.query('SELECT count(id) FROM "post"')
+            const postIdArr = await db.query('SELECT id FROM "post" ORDER BY id')
+            for (var i = 0; i < postCount.rows[0]['count']; i++) {
+                await db.query('UPDATE "post" SET id = $1 WHERE id = $2', [i, postIdArr.rows[i]['id']])
+            }
+            res.json({code: 0, text: 'Пост удалён'})
         } else {
-
+            res.json({code: 1, text: 'Пост с таким id не найден'})
         }
-        
+    }
+
+    async getPostById(req, res) {
+        const id = req.params.id
+        const isTherePost = await db.query('select id, user_id, to_char(publicationdate,\'dd-mm-yyyy\') as publicationdate, text from "post" where id = $1', [id])
+        if (isTherePost.rowCount == 1) {
+            res.json({code: 0, text: 'Пост найден', data: isTherePost.rows[0]})
+        } else {
+            res.json({code: 1, text: 'Пост с таким id не найден'})
+        }
+    }
+
+    async getPostsByUserId(req, res) {
+        const userId = req.params.userId
+        const isTherePost = await db.query('select id, user_id, to_char(publicationdate,\'dd-mm-yyyy\') as publicationdate, text from "post" where user_id = $1', [userId])
+        if (isTherePost.rowCount == 0) {
+            res.json({code: 1, text: 'У пользователя нет постов'})
+        } else {
+            res.json({code: 0, text: 'Пост найден', data: isTherePost.rows})
+        }
+    }
+
+    async getPosts(req, res) {
+        const posts = await db.query('select id, user_id, to_char(publicationdate,\'dd-mm-yyyy\') as publicationdate, text from "post"')
+        if (posts.rowCount == 0) {
+            res.json({code: 1, text: 'Постов нет'})
+        } else {
+            res.json({code: 0, text: 'Посты найдены', data: posts.rows})
+        }
+    }
+    async getPostsOrderByDateASC(req, res) {
+        const posts = await db.query('select id, user_id, to_char(publicationdate,\'dd-mm-yyyy\') as publicationdate, text from "post" ORDER BY "publicationdate" asc')
+        if (posts.rowCount == 0) {
+            res.json({code: 1, text: 'Постов нет'})
+        } else {
+            res.json({code: 0, text: 'Посты найдены', data: posts.rows})
+        }
+    }
+    async getPostsOrderByDateDESC(req, res) {
+        const posts = await db.query('select id, user_id, to_char(publicationdate,\'dd-mm-yyyy\') as publicationdate, text from "post" ORDER BY "publicationdate" desc')
+        if (posts.rowCount == 0) {
+            res.json({code: 1, text: 'Постов нет'})
+        } else {
+            res.json({code: 0, text: 'Посты найдены', data: posts.rows})
+        }
+    }
+
+
+
+    async getPostByUserId(req, res) {
+
     }
 
     async test(req, res) {
